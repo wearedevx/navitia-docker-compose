3df9c5aab266a5095e3c3f4f7e736342afe50c80
Antoine Desbordes <antoine.desbordes@gmail.com>
2016-11-22 09:51:52 +0100

    add an instance configurator

---
+++ instances_configurator/instances_configuration.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+tyr_config() {
+  instance_name=$1
+  INSTANCE=$instance_name envsubst < templates/tyr_instance.ini > /etc/tyr.d/$instance_name.ini
+
+  mkdir -p /srv/ed/$instance_name
+}
+
+jormungandr_config() {
+  instance_name=$1
+
+}
+
+db_config() {
+  instance_name=$1
+
+  # wait for db ready
+  while ! pg_isready --host=database; do
+    echo "waiting for postgres to be ready"
+    sleep 1;
+  done
+
+  # database creation
+  PGPASSWORD=navitia createdb --host database -U navitia $instance_name
+  PGPASSWORD=navitia psql -c 'CREATE EXTENSION postgis;' --host database $instance_name navitia
+
+  # database schema migration
+  alembic_file=/srv/ed/$instance_name/ed_migration.ini
+  INSTANCE=$instance_name envsubst < templates/ed_migration.ini > $alembic_file 
+  alembic -c $alembic_file upgrade head
+}
+
+add_instance() {
+  instance_name=$1
+
+  # tyr configuration
+  tyr_config $instance_name
+
+  # jormungandr configuration
+  jormungandr_config $instance_name
+
+  # db creation and migration
+  db_config $instance_name
+}
+
+# TODO discover the instance automaticaly
+add_instance 'default'
--- tyr_beat/settings.py
+++ tyr_beat/settings.py
@@ -3,3 +3,5 @@ CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
 SQLALCHEMY_DATABASE_URI = 'postgresql://navitia:navitia@database/jormungandr'
 
 CITIES_DATABASE_URI = None
+
+INSTANCES_DIR = '/etc/tyr.d'
