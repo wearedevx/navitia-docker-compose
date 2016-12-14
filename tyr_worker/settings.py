03fab635a38d03d79b67cc3e4a6fa4366dd1385d
antoine-de <antoine.desbordes@gmail.com>
2016-12-14 09:48:24 +0100

    remove old files

--- tyr_worker/settings.py
+++
@@ -1,9 +0,0 @@
-CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
-
-SQLALCHEMY_DATABASE_URI = 'postgresql://navitia:navitia@database/jormungandr'
-
-CITIES_DATABASE_URI = None
-
-REDIS_HOST = 'redis'
-
-INSTANCES_DIR = '/etc/tyr.d'
