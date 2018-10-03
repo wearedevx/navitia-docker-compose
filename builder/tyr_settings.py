ce171cec43660852914404b60ffadfba539dc470
mbounabi <Mehdi.Bounabi@Kisio.org>
2018-10-03 16:48:40 +0200

    After Review

--- builder/tyr_settings.py
+++ builder/tyr_settings.py
@@ -2,8 +2,8 @@ CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://navitia:navitia@database/jormungandr'
 
-# TODO: If the presence of the cities db is not needed, un-comment this line
-#CITIES_DATABASE_URI = None
+# If the presence of the cities db is not needed, un-comment the following line
+# CITIES_DATABASE_URI = None
 
 REDIS_HOST = 'redis'
 
