ea41b8d97edae9fd0a16ba8bde16066acc7e5a47
Kévin <kevin@wearedevx.com>
2020-03-18 07:27:58 +0100

    increase payload size

--- update-server/index.js
+++ update-server/index.js
@@ -1,33 +1,37 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require("express")
+const bodyParser = require("body-parser")
 
-const updater = require("./src/index");
+const updater = require("./src/index")
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "500mb" }))
+
+console.log('OK NEW VERSION !!')
+
+const { AUTH_TOKEN } = process.env
 
 function simpleAuthCheck(req, res, next) {
-  const { AUTH_TOKEN } = process.env;
-
-  const token = (req.headers.authorization || "").replace("Bearer ", "");
+  const token = (req.headers.authorization || "").replace("Bearer ", "")
 
   if (!token || token === "" || token !== AUTH_TOKEN) {
-    res.status(401).send("Unauthorized");
+    res
+      .status(401)
+      .send(`Unauthorized: ${AUTH_TOKEN} / ${req.headers.authorization}`)
   } else {
-    next();
+    next()
   }
 }
 
-app.use(simpleAuthCheck);
+app.use(simpleAuthCheck)
 
 // The actual request handler
 app.post("/update", (req, res) => {
-  updater(req, res);
-});
+  updater(req, res)
+})
 
-const port = process.argv[2] || 5000;
+const port = process.argv[2] || 5000
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+  console.log(`App listening on port ${port}`)
+})
